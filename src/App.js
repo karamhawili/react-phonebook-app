3230e68acc487bd931f01ef648cd8271c499c460
Karam AbuHawili <karam.hawili2@gmail.com>
2022-09-26 04:32:40 +0300

    fetch data from server

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
-//Phonebook, 2.6 - 2.10, FSO
-
-//2.6 addition of a person to phonebook
+//Phonebook, 2.6 - 2.11, FSO
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3002/persons').then((response) => {
+      console.log('promise fulfilled');
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
